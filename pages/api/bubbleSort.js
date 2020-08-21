051bd2c911783fac427b45364b45a89c4e846c98
Alvine <alvine@tamkin.se>
2020-08-21 13:30:36 +0200

    bubble sort function

--- pages/api/bubbleSort.js
+++ pages/api/bubbleSort.js
@@ -1,10 +1,36 @@
-/*import { NowRequest, NowResponse} from '@vercel/node'
+const bubbleSort = () => {
+  return new Promise((resolve, reject) => {
 
-export default(req: NowRequest, res: NowRespone) => {
-  res.json(50)
-}*/
+    var array = []
+    for(let i = 0; i <= items; i++) {
+      array.push(Math.floor(Math.random() * Math.floor(items)))
+    }
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+    const start = performance.now()
+    for(let i = 0; i < array.length - 1; i++) {
+      for(let y = 0; y < array.length - 1 - i; y++) {
+        if(array[y] > array[y + 1]) {
+          var temp = array[y]
+          array[y] = array[y + 1]
+          array[y + 1] = temp
+        }
+      }
+    }
+    const end = performance.now()
+    resolve( ((end-start) * 0.001).toFixed(2) )
+
+  })
+}
+
+export default async (req, res) => {
+
+  try {
+    const result = await bubbleSort()
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(result))
+  } catch {
+    console.log(error)
+  }
+
 }

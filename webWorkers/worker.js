a9eb8fa44d0c91a517300323d4c9579f42bb4c8a
Alvine <alvine@tamkin.se>
2020-08-22 19:53:16 +0200

    web worker

--- webWorkers/worker.js
+++ webWorkers/worker.js
@@ -2,14 +2,18 @@ export default () => {
 	self.addEventListener('message', e => { // eslint-disable-line no-restricted-globals
 		if (!e) return;
 
+    console.log('In the web worker')
+
     const url = 'https://jam.alvineandersson.se/api/bubbleSort';
 
     fetch(url).then(res => {
+      console.log('res', res)
       res.json()
     }).then(data => {
+      console.log('data', data)
       postMessage(data)
     }).catch(err => {
-      console.log(err)
+      console.log('err', err)
     })
 
 	})
